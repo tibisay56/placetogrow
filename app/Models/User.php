97086c00b2e9708b23ffb0a6b7e3480e1b08e6eb
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-08-12 16:42:33 -0500

    fix: update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -35,5 +36,10 @@ class User extends Authenticatable
     public function site(): HasOne
     {
         return $this->hasOne(Site::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
     }
 }
