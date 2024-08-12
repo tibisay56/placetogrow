9871fbf185a2a2da2dfe190eaf6535db191bc001
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-08-12 17:52:21 -0500

    chore: update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TypeSeeder::class,
             SitesSeeder::class,
-            PaymentSeeder::class,
         ]);
     }
 }
