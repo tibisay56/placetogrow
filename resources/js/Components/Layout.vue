a5d4af50e8911b67ea652f0208656b52f9bfdcd4
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-08-23 14:43:17 -0500

    fix: adjust layout for subscription plan UI

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -201,6 +201,14 @@ const toggleDropdown = () => {
                             </button>
                             </Link>
                         </li>
+                        <li class="hs-accordion" id="projects-accordion">
+                            <Link :href="route('subscription.index')">
+                                <button type="button" class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="projects-accordion-child">
+                                    <font-awesome-icon :icon="['far', 'address-card']" />
+                                    {{ $t('Subscriptions') }}
+                                </button>
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
