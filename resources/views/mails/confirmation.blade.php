e2fe9a5c2a11e5977da35d344180311182960371
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-08 17:49:45 -0500

    feat: Add mail views

---
+++ resources/views/mails/confirmation.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Plan Confirmation</title>
+</head>
+<body>
+<h1>Thank you for subscribing!</h1>
+<p>Your email: {{ $email }}</p>
+<p>Your reference: {{ $reference }}</p>
+<p>We're excited to have you on board!</p>
+</body>
+</html>
