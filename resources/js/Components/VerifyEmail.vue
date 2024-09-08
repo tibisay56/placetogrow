a63ee26f67c4bcddeffe73b766a37ba8e5657b03
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-08 17:49:00 -0500

    feat: Add subscription pages

---
+++ resources/js/Components/VerifyEmail.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {usePage} from "@inertiajs/vue3";
+
+
+const { props } = usePage();
+const email = props.value?.email || '';
+console.log(props);
+
+</script>
+
+<template>
+    <font-awesome-icon :icon="['fas', 'check-to-slot']" class="mt-10 text-4xl text-orange-500"/>
+    <h1 class="mt-10 text-xl text-gray-800 font-bold md:text-2xl md:leading-tight lg:text-3xl lg:leading-tight dark:text-neutral-200">
+        Click the link we sent to <br/> your email: <span class="text-orange-600 dark:text-orange-500">{{ email }}</span> <br/>
+        to complete the verification
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Subscription/Index.vue
@@ -0,0 +1,215 @@
+<script setup>
+import {usePage, useForm, Link} from '@inertiajs/vue3';
+import {computed, onMounted, ref, watch} from "vue";
+import Layout from "@/Components/Layout.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+
+
+const { props } = usePage();
+const subscriptions = ref(props.subscriptions || []);
+
+const colorBySubscriptionStatus = {
+    active: 'bg-green-100 text-green-800 dark:bg-green-500/10 dark:text-green-500',
+    inactive: 'bg-blue-100 text-blue-800 dark:bg-blue-500/10 dark:text-blue-500',
+    pending: 'bg-yellow-100 text-yellow-800 dark:bg-yellow-500/10 dark:text-yellow-500',
+};
+const getBadgeClasses = (subscriptionStatus) => {
+    return colorBySubscriptionStatus[subscriptionStatus] || 'bg-gray-100 text-gray-800 dark:bg-gray-500/10 dark:text-gray-500';
+};
+
+const onDeleteSuccess = (e) => {
+    console.log(e)
+    subscriptions.value = e.props.subscriptions || [];
+}
+
+</script>
+
+<template>
+    <Layout></Layout>
+    <!-- Content -->
+    <div class="w-full lg:ps-64 -mt-8">
+        <div class="p-4 sm:p-6 space-y-4 sm:space-y-6">
+            <!-- Card -->
+            <div class="flex flex-col">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                        <div class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-neutral-800 dark:border-neutral-700">
+                            <!-- Header -->
+                            <div class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-neutral-700">
+                                <div>
+                                    <h2 class="text-xl font-semibold text-gray-800 dark:text-neutral-200">
+                                        {{ $t('Subscriptions') }}
+                                    </h2>
+                                    <p class="text-sm text-gray-600 dark:text-neutral-400">
+                                        {{ $t('Add Subscriptions, edit and more.') }}
+                                    </p>
+                                </div>
+
+                                <div>
+                                    <div class="inline-flex gap-x-2">
+                                        <Link :href="route('subscription.index')">
+                                            <a class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" href="#">
+                                                {{ $t('View all') }}
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Header -->
+                            <!-- Table -->
+                            <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                                <thead class="bg-gray-50 dark:bg-neutral-800">
+                                <tr>
+                                    <th scope="col" class="ps-6 py-3 text-start">
+                                        <label for="hs-at-with-checkboxes-main" class="flex">
+                                            <input type="checkbox" class="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-main">
+                                            <span class="sr-only">Checkbox</span>
+                                        </label>
+                                    </th>
+                                    <th scope="col" class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Plan') }}
+                                              </span>
+                                        </div>
+                                    </th>
+
+                                    <th scope="col" class="px-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Description') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Amount') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Status') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Frequency') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Subscription Expiration') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-end">
+                                        <div class="flex items-center gap-x-2">
+                                              <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                                                {{ $t('Actions') }}
+                                              </span>
+                                        </div>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                                <tr v-for="subscription in subscriptions" :key="subscription.id">
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="ps-6 py-3">
+                                            <label for="hs-at-with-checkboxes-1" class="flex">
+                                                <input type="checkbox" class="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-1">
+                                                <span class="sr-only">Checkbox</span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="px-6 py-3">
+                                            <span class="block text-sm text-gray-500 dark:text-neutral-500">  {{ subscription.plan.name }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="px-6 py-3">
+                                            <span class="block text-sm text-gray-500 dark:text-neutral-500">  {{ subscription.plan.description }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="px-6 py-3">
+                                            <span class="block text-sm text-gray-500 dark:text-neutral-500">  {{ subscription.plan.amount }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
+                                            <div class="flex items-center gap-x-3">
+                                                <div class="grow">
+                                                        <span :class="`capitalize py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium rounded-full ${getBadgeClasses(subscription.status ? subscription.status : 'Unknown')}`">
+                                                            {{ subscription.status }}</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="px-6 py-3">
+                                            <span class="block text-sm font-semibold text-gray-800 dark:text-neutral-200">  {{ subscription.plan.billing_frequency }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <div class="px-6 py-3">
+                                            <span class="block text-sm text-gray-500 dark:text-neutral-500">  {{ subscription.plan.subscription_expiration }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="size-px whitespace-nowrap">
+                                        <Dropdown align="right" width="48">
+                                            <template #trigger>
+                                        <span class="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                {{ $t('Actions') }}
+
+                                                <svg
+                                                    class="ms-2 -me-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clip-rule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                            </template>
+                                            <template #content>
+                                                <DropdownLink :href="route('subscription.show', subscription)">{{ $t('Show') }}</DropdownLink>
+                                                <DropdownLink  @success="onDeleteSuccess" :href="route('subscription.destroy', subscription)" method="delete" as="button">
+                                                    {{ $t('Delete') }}
+                                                </DropdownLink>
+                                            </template>
+                                        </Dropdown>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
+
