7473346f3eaec4ae7f7a4cbe445d32e40b44adf4
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-08 17:31:28 -0500

    feat: Enhance site slug page

--- resources/js/Pages/Site/Slug.vue
+++ resources/js/Pages/Site/Slug.vue
@@ -1,8 +1,12 @@
 <script setup>
-import { Head, useForm, usePage } from '@inertiajs/vue3';
+import {Head, Link, router, useForm, usePage} from '@inertiajs/vue3';
 import InputError from "@/Components/InputError.vue";
-import {ref} from 'vue';
+import {onMounted, reactive, ref} from 'vue';
 import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Stepper from "@/Components/Stepper.vue";
+import {loadLanguageAsync} from "laravel-vue-i18n";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import Dropdown from "@/Components/Dropdown.vue";
 
 const { props } = usePage();
 const documentTypes = ref(props.documentTypes || []);
@@ -32,33 +36,130 @@ const goBack = () => {
 
 const submit = () => {
     form.post(route('payment.store'), {
-        onError: (errors) => {
-            console.log('Errores de validación:', errors);
-        },
         onSuccess: () => {
-            console.log('Formulario enviado con éxito');
+            console.log('Form submitted successfully');
         }
     });
 };
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+//Language
+
+const langTitle = ref (localStorage.getItem('langTitle') || 'English')
+const items= reactive({
+    item: [
+        {title: 'English', value:'en'},
+        {title: 'Spanish', value:'es'},
+    ]
+})
+
+const changeLocale = async (item) => {
+    localStorage.setItem('langTitle', item.title);
+    await router.get(route('language', item.value));
+    localStorage.setItem('lang', item.value);
+    await loadLanguageAsync(item.value);
+};
+
+onMounted(() => {
+    if (site.value.type && site.value.type.name === 'subscriptions') {
+        const url = route('subscription.create', { siteId: site.value.id });
+        window.location.href = url;
+    }
+});
 
 </script>
 
 <template>
     <Head title="Show Sites" />
-    <div class="max-w-[85rem] px-6 py-10 sm:px-8 lg:px-10 lg:py-14 mx-auto">
-        <div class="p-4 bg-white rounded-lg shadow-md dark:bg-neutral-800 mx-12 lg:mx-64">
+    <header class="fixed top-0 left-0 w-full bg-white border-b border-gray-100 dark:bg-black dark:border-neutral-700 z-50">
+        <div class="relative w-full max-w-9xl px-4 lg:px-6 py-2 flex items-center justify-between">
+            <div class="flex items-center">
+                <img src="/Documents/logo.png" alt="Logo" class="h-10 w-auto">
+            </div>
+            <div class="ms-20 relative max-w-xs">
+                <label class="sr-only">Search</label>
+                <input type="text" name="hs-table-with-pagination-search" id="hs-table-with-pagination-search" class="py-2 px-3 ps-9 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search for items">
+                <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3">
+                    <svg class="size-4 text-gray-400 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </svg>
+                </div>
+            </div>
+            <div class="flex-grow"></div>
+            <div class="ms-3 relative">
+                <Dropdown align="right" width="48">
+                    <template #trigger>
+                            <span class="inline-flex rounded-md">
+                                <button type="button" class="inline-flex items-center px-10 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    {{ langTitle }}
+                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/>
+                                    </svg>
+                                </button>
+                            </span>
+                    </template>
+                    <template #content>
+                        <DropdownLink href="#" v-for="(item, index) in items.item" :key="index" @click="changeLocale(item)">
+                            {{ item.title }}
+                        </DropdownLink>
+                    </template>
+                </Dropdown>
+            </div>
+            <nav v-if="canLogin" class="flex items-center space-x-2 ml-auto">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="rounded-md px-2 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                    Dashboard
+                </Link>
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="rounded-md px-2 py-2 text-gray-500 bg-white hover:text-gray-700 ring-1 ring-transparent transition focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                        {{ $t('Login') }}
+                    </Link>
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="rounded-md px-2 py-2 text-gray-500 bg-white hover:text-gray-700 ring-1 ring-transparent transition focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                        {{ $t('Register') }}
+                    </Link>
+                </template>
+            </nav>
+        </div>
+    </header>
+    <div class="max-w-full px-6 py-10 sm:px-8 lg:px-10 lg:py-14 mt-10">
+        <div class="p-4 bg-white rounded-lg shadow-md dark:bg-neutral-800 mx-12 lg:mx-12">
             <div v-if="site.avatar" class="flex justify-center items-center h-20">
                 <img class="mx-auto flex flex-col items-center rounded-lg border border-gray-300 shadow mb-10" width="100" height="100" :src="`/storage/${site.avatar}`" alt="avatar"/>
             </div>
             <div>
                 <h1 v-if="site.name" class="text-xl font-semibold text-gray-800 dark:text-neutral-200"> {{ site.name }}</h1>
-                <p v-if="site.category" class="text-sm text-gray-600 dark:text-neutral-400 mb-4"> {{ site.category }}</p>
-                <p v-if="site.type" class="capitalize text-sm text-gray-600 dark:text-neutral-400 mb-4"> {{ site.type.name }}</p>
+                <p v-if="site.category" class="text-sm text-gray-600 dark:text-neutral-400 mb-2"> {{ site.category }}</p>
+                <p v-if="site.type" class="capitalize text-sm text-gray-600 dark:text-neutral-400"> {{ $t(site.type.name) }}</p>
             </div>
-            <div class="space-y-8">
+            <div v-if="site.type && site.type.name === 'subscriptions'">
+            </div>
+            <div  v-if="site.type.name !== 'subscriptions'"  class="space-y-8">
                 <div class="bg-white dark:bg-neutral-800 p-4 border border-gray-200 rounded-lg">
                     <form @submit.prevent="submit">
-
                         <div v-if="form.required_Fields.length" class="flex flex-col sm:flex-row gap-4">
                             <label class="inline-block text-sm font-medium dark:text-white">Required Fields:</label>
                             <div v-for="(field, index) in form.required_Fields" :key="index" class="mb-2">
