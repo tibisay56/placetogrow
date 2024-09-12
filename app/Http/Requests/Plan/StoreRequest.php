d606305d3f290ca66118a12a4ed3a63513ae760c
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-11 20:48:35 -0500

    add validation name

--- app/Http/Requests/Plan/StoreRequest.php
+++ app/Http/Requests/Plan/StoreRequest.php
@@ -20,7 +20,7 @@ class StoreRequest extends FormRequest
             'plan_type_id' => 'required|exists:plan_types,id',
             'amount' => 'required|numeric|min:0',
             'currency' => 'required',
-            'description' => 'nullable|string',
+            'description' => 'required|string',
             'subscription_expiration' => 'required|integer|min:1|max:1440',
             'site_id' => 'required|exists:sites,id',
             'billing_frequency' => 'required|in:'.implode(',', BillingFrequency::toArray()),
