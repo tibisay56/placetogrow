5ea3259274a58c0b378c4b63fda22cabdc378d1a
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-16 16:26:24 -0500

    chore(database): update SiteFactory and SitesSeeder to adjust site data

--- database/factories/SiteFactory.php
+++ database/factories/SiteFactory.php
@@ -24,7 +24,27 @@ class SiteFactory extends Factory
             'payment_expiration_time' => $this->faker->numberBetween(1, 1440),
             'required_fields' => [],
             'type_id' => $this->faker->randomElement($typeIds),
+            'avatar' => $this->generateRandomImage(),
+
         ];
+    }
+
+    /**
+     * Generate a random image filename.
+     *
+     * @return string
+     */
+    protected function generateRandomImage()
+    {
+        $imageNames = [
+            'default_avatar.png',
+            'image_1.png',
+            'image_2.png',
+        ];
+
+        $imageName = $this->faker->randomElement($imageNames);
+
+        return $imageName;
     }
 
     public function subscriptionType(): self
