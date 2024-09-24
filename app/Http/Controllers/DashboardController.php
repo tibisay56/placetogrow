e251574a5e2084580818f0257e979187cdd10bb5
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-24 14:35:27 -0500

    feat(dashboard): add new feature to DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use App\Models\User;
 use Inertia\Inertia;
 
@@ -11,16 +12,24 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
 
-        if ($user instanceof User) {
-            return Inertia::render('Dashboard', [
-                'user' => $user,
-                'roles' => $user->roles,
-            ]);
-        }
+        $totalPaidInvoices = Invoice::where('status', 'paid')->count();
+        $totalPendingInvoices = Invoice::where('status', 'pending')->count();
+        $totalOverdueInvoices = Invoice::where('status', 'overdue')->count();
+
+        $totalPaidAmount = Invoice::where('status', 'paid')->sum('amount');
+        $totalPendingAmount = Invoice::where('status', 'pending')->sum('amount');
+        $totalOverdueAmount = Invoice::where('status', 'overdue')->sum('amount');
 
         return Inertia::render('Dashboard', [
             'user' => $user,
-            'roles' => [],
+            'roles' => $user ? $user->roles : [],
+            'totalpaidInvoices' => $totalPaidInvoices,
+            'totalpendingInvoices' => $totalPendingInvoices,
+            'totaloverdueInvoices' => $totalOverdueInvoices,
+
+            'totalPaidAmount' => $totalPaidAmount,
+            'totalPendingAmount' => $totalPendingAmount,
+            'totalOverdueAmount' => $totalOverdueAmount,
         ]);
     }
 }
