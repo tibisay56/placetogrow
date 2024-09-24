b62d6c66275030a35aea1c406d3fb01cd13b0a76
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-24 14:55:41 -0500

    feat: Add site retrieval logic in SubscriptionFactory for type_id 3

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Constants\DocumentTypes;
 use App\Constants\SubscriptionStatus;
 use App\Models\Plan;
+use App\Models\Site;
 use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -15,9 +16,18 @@ class SubscriptionFactory extends Factory
 
     public function definition(): array
     {
+        $site = Site::where('type_id', 3)->inRandomOrder()->first();
+
+        if (! $site) {
+            $site = Site::factory()->create([
+                'type_id' => 3,
+            ]);
+        }
+
         return [
             'user_id' => User::factory(),
             'plan_id' => Plan::factory(),
+            'site_id' => $site->id,
             'status' => $this->faker->randomElement(SubscriptionStatus::toArray()),
             'email' => $this->faker->unique()->safeEmail(),
             'name' => $this->faker->word(),
