1220f8e220e5c8d5766ca3cffa90c8024469ecce
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-09-24 14:53:38 -0500

    feat: Add invoices relationship to the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Subscription::class);
     }
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
