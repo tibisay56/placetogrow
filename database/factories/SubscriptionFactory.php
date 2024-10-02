a1b36cbe3f5c892a47243de0b05ed343c377b73c
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-02 18:25:36 -0500

    feat: add Str facade

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -9,6 +9,7 @@ use App\Models\Site;
 use App\Models\Subscription;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class SubscriptionFactory extends Factory
 {
@@ -16,23 +17,28 @@ class SubscriptionFactory extends Factory
 
     public function definition(): array
     {
-        $site = Site::where('type_id', 3)->inRandomOrder()->first();
-
-        if (! $site) {
-            $site = Site::factory()->create([
-                'type_id' => 3,
-            ]);
-        }
 
         return [
             'user_id' => User::factory(),
             'plan_id' => Plan::factory(),
-            'site_id' => $site->id,
+            'site_id' => Site::factory()->create(['type_id' => 3])->id,
             'status' => $this->faker->randomElement(SubscriptionStatus::toArray()),
             'email' => $this->faker->unique()->safeEmail(),
             'name' => $this->faker->word(),
             'document_number' => $this->faker->unique()->numerify('##########'),
             'document_type' => $this->faker->randomElement(DocumentTypes::toArray()),
+            'token' => Str::random(70),
+            'sub_token' => Str::random(50),
+            'next_payment_at' => now()->toDateString(),
         ];
+    }
+
+    public function statusApproved(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => SubscriptionStatus::APPROVED,
+            ];
+        });
     }
 }
