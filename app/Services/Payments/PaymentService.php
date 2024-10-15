6b5eaab2b284b98f038fae86edcdca8c24f2d102
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-15 17:54:31 -0500

    feat: implement payment collection method in PaymentService

--- app/Services/Payments/PaymentService.php
+++ app/Services/Payments/PaymentService.php
@@ -2,9 +2,14 @@
 
 namespace App\Services\Payments;
 
+use App\Constants\PaymentStatus;
 use App\Contracts\PaymentGateway;
 use App\Contracts\PaymentService as PaymentServiceContract;
 use App\Models\Payment;
+use App\Models\Subscription;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class PaymentService implements PaymentServiceContract
 {
@@ -12,6 +17,33 @@ class PaymentService implements PaymentServiceContract
         protected Payment $payment,
         protected PaymentGateway $gateway,
     ) {
+    }
+
+    public function collect(Subscription $subscription, User $user)
+    {
+        $payment = Payment::create([
+            'reference' => now()->format('ymdHis').'-'.strtoupper(Str::random(4)),
+            'subscription_id' => $subscription->id,
+            'user_id' => $user->id,
+            'amount' => $subscription->plan->amount,
+            'currency' => $subscription->plan->currency,
+            'description' => $subscription->plan->description,
+            'status' => PaymentStatus::APPROVED->value,
+            'site_id' => $subscription->site->id,
+            'collect' => true,
+        ]);
+
+        $response = $this->gateway->prepare()
+            ->payment($payment)
+            ->process();
+
+        $payment->update([
+            'process_identifier' => $response->processIdentifier,
+            'status' => $response->status,
+            'collect' => true,
+        ]);
+
+        return $payment;
     }
 
     public function create(array $buyer): PaymentResponse
@@ -32,6 +64,10 @@ class PaymentService implements PaymentServiceContract
     {
         $response = $this->gateway->prepare()
             ->get($this->payment);
+        Log::info('Payment status response from gateway', [
+            'payment_id' => $this->payment->id,
+            'response' => $response,
+        ]);
 
         return tap($this->payment)->update([
             'status' => $response->status,
