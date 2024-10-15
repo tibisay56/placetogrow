f15af542d738a8abe15daf035cf4043628d2b088
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-15 17:42:11 -0500

    Refactor: Added logic to create additional invoices

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Invoice;
+use App\Models\Site;
 use Illuminate\Database\Seeder;
 
 class InvoiceSeeder extends Seeder
@@ -12,6 +13,19 @@ class InvoiceSeeder extends Seeder
      */
     public function run(): void
     {
-        Invoice::factory()->count(100)->create();
+        $sites = Site::all();
+
+        foreach ($sites as $site) {
+            Invoice::factory()->count(5)->create([
+                'site_id' => $site->id,
+            ]);
+        }
+
+        $invoicesCreated = Invoice::count();
+
+        if ($invoicesCreated < 100) {
+            $remainingInvoices = 100 - $invoicesCreated;
+            Invoice::factory()->count($remainingInvoices)->create();
+        }
     }
 }
