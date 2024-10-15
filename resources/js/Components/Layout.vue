e4f149e27ca2ef94c91e408f94f1b2a5da1959b5
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-15 17:45:58 -0500

    feat: add Metrics navigation link for Admin users

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -166,7 +166,14 @@ const toggleDropdown = () => {
                                 {{ $t('Dashboard') }}
                             </a>
                         </li>
-
+                        <li v-show="hasRole('Admin')">
+                            <Link :href="route('metrics.index')">
+                                <button type="button" class="w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="users-accordion-child">
+                                    <font-awesome-icon :icon="['fas', 'chart-simple']" />
+                                    {{ $t('Metrics') }}
+                                </button>
+                            </Link>
+                        </li>
                         <li>
                             <Link :href="route('payment.index')">
                                 <button type="button" class="w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="users-accordion-child">
