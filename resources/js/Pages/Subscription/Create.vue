f5b8a9f4f1f3f6dca47cddafad16c744578df04a
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-17 17:46:36 -0500

    feat: add surname field to subscription form

--- resources/js/Pages/Subscription/Create.vue
+++ resources/js/Pages/Subscription/Create.vue
@@ -41,6 +41,7 @@ const canProceedToNextStep = computed(() => {
 
 const form = useForm({
     name:'',
+    surname:'',
     email: '',
     document_number: '',
     document_type: documentTypes.value[0] || null,
@@ -266,6 +267,17 @@ const submitForm = () => {
                                                 </div>
                                             </div>
                                             <InputError class="mt-2" :message="form.errors.name" />
+                                        </div>
+                                        <div>
+                                            <div class="relative">
+                                                <input type="text" id="surname" name="surname"  v-model="form.surname"  placeholder="SurName" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="name-error">
+                                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <InputError class="mt-2" :message="form.errors.surname" />
                                         </div>
                                         <div class="relative">
                                             <select v-model="form.document_type" id="document_type" name="document_type" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 mb-4">
