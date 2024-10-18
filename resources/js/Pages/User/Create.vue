8267d72afb89abe481da5d7dbe961c75e1cb6b9c
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-18 18:34:40 -0500

    fix: update localization for 'Photo' label in User Create view

--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -85,7 +85,7 @@ const submit = () => {
                                                     <InputError :message="form.errors.password_confirmation" class="mt-2" />
                                                 </div>
                                                 <form class="max-w-sm">
-                                                    <InputLabel for="photo" value="Photo" />
+                                                    <InputLabel for="photo" :value="$t('Photo')" />
                                                     <FileInput name="photo" @change="onSelectPhoto" class="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
                                                                 file:bg-gray-50 file:border-0
                                                                 file:me-4
