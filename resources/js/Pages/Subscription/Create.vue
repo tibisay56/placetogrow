0e8e3c861b0112445f0f494c9733c49f468f3fdf
Tibisay Barroso <tibisaybarroso56@gmail.com>
2024-10-19 10:57:31 -0500

    refactor: make email placeholder translatable in form

--- resources/js/Pages/Subscription/Create.vue
+++ resources/js/Pages/Subscription/Create.vue
@@ -220,7 +220,7 @@ const submitForm = () => {
                                             <div class="grid gap-y-4">
                                                 <div>
                                                     <div class="relative">
-                                                        <input type="email" id="email" name="email"  v-model="form.email"  placeholder="Email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="email-error">
+                                                        <input type="email" id="email" name="email"  v-model="form.email"  :placeholder="$t('Email')" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="email-error">
                                                         <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                                             <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -232,7 +232,7 @@ const submitForm = () => {
                                                 <!-- End Form Group -->
                                                 <div>
                                                     <div class="relative">
-                                                        <input type="password" id="password" name="password"  v-model="form.password"  placeholder="Password" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="password-error">
+                                                        <input type="password" id="password" name="password"  v-model="form.password"  :placeholder="$t('Password')" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="password-error">
                                                         <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                                             <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -259,7 +259,7 @@ const submitForm = () => {
                                         <!-- Form Group -->
                                         <div>
                                             <div class="relative">
-                                                <input type="text" id="name" name="name"  v-model="form.name"  placeholder="Name" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="name-error">
+                                                <input type="text" id="name" name="name"  v-model="form.name"  :placeholder="$t('Name')" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="name-error">
                                                 <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                                     <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -270,7 +270,7 @@ const submitForm = () => {
                                         </div>
                                         <div>
                                             <div class="relative">
-                                                <input type="text" id="surname" name="surname"  v-model="form.surname"  placeholder="SurName" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="name-error">
+                                                <input type="text" id="surname" name="surname"  v-model="form.surname"  :placeholder="$t('Last Name')" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" required aria-describedby="name-error">
                                                 <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                                     <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -284,7 +284,7 @@ const submitForm = () => {
                                                 <option :value="null">Select option...</option>
                                                 <option v-for="documentType in documentTypes" :key="documentType" :value="documentType">{{ documentType }}</option>
                                             </select><InputError :message="form.errors.documentType" class="mb-8"/>
-                                            <input v-model="form.document_number" type="number" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Document Number"><InputError :message="form.errors.document_number" class="mt-2" />
+                                            <input v-model="form.document_number" type="number" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" :placeholder="$t('Document Number')"><InputError :message="form.errors.document_number" class="mt-2" />
                                         </div>
                                         <div v-if="form.required_Fields.length" class="relative">
                                             <div v-for="(field, index) in form.required_Fields" :key="index" class="mb-2">
